5390b1a115e51c1ad0143ad44eee0c87aefc8c68
AwesomerX <72046415+AwesomerX@users.noreply.github.com>
2021-09-24 15:36:51 -0500

    Update practice

--- Locators.py
+++ Locators.py
@@ -9,11 +9,11 @@ Forgot your password:  search  (By.ID,  'auth-fpp-link')
 
 Other issues with Sign-In:  search  (By.XPATH,  ("//a[@id='ap-other-signin-issues-link']")
 
- Create your Amazon account:  search  (By.ID,  'createAccountSubmit')
+Create your Amazon account:  search  (By.ID,  'createAccountSubmit')
 
- *Conditions of use link:  search  (BY.XPATH,  ("//a[text()='Conditions of Use")
+*Conditions of use link:  search  (BY.XPATH,  ("//a[text()='Conditions of Use")
 
- *Privacy Notice link:  search  (By.XPATH,  ("//a[text()='Privacy Notice']")
+*Privacy Notice link:  search  (By.XPATH,  ("//a[text()='Privacy Notice']")
 
 
 
