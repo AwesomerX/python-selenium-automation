93926d149580a9a6daf220a911efccaca9f00158
AwesomerX <72046415+AwesomerX@users.noreply.github.com>
2021-09-23 22:37:42 -0500

    Adding Locators HW

---
+++ Locators.py
@@ -0,0 +1,20 @@
+"""
+Amazon Logo: search  (By.XPATH,  ("//i[@class='a-icon a-icon-logo']")
+
+Continue Button: search  (By.ID,  'Continue')
+
+Need Help Link:  search  (By.XPATH, ("//span[@class='a-expander-prompt']")
+
+Forgot your password:  search  (By.ID,  'auth-fpp-link')
+
+Other issues with Sign-In:  search  (By.XPATH,  ("//a[@id='ap-other-signin-issues-link']")
+
+ Create your Amazon account:  search  (By.ID,  'createAccountSubmit')
+
+ *Conditions of use link:  search  (BY.XPATH,  ("//a[text()='Conditions of Use")
+
+ *Privacy Notice link:  search  (By.XPATH,  ("//a[text()='Privacy Notice']")
+
+
+
+"""
